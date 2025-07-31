24ff66055f17a20d70f2325c1361c590bb70dbad
MS. NATTAWADEE WUTTIVORADIT <pattynp2005@gmail.com>
2025-07-31 13:50:24 +0700

    Fix giveMedicine function to ensure dropPills is called correctly before processing pills

--- server/services/medServices.js
+++ server/services/medServices.js
@@ -199,7 +199,7 @@ export const giveMedicine = async (allergies, symptomIds = [], medicineIds = [])
     return []; // Return empty array instead of throwing
   }
 
-  // await dropPills(pills.map(pill => pill.medicine_id));
+  await dropPills(pills.map(pill => pill.medicine_id));
   for (const pill of pills) {
     await removeStock(pill.medicine_id, pill.amount);
     const data = await getPillData(pill);
